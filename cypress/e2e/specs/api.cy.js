2824db42707c09d175346d39cf05a5828b6f98aa
Hrqlv <henriquelopesvelozo@gmail.com>
2024-06-20 15:36:32 -0300

    Feito o test de api para pegar os usuarios - GET

--- cypress/e2e/specs/api.cy.js
+++ cypress/e2e/specs/api.cy.js
@@ -1,10 +1,15 @@
 /// <reference types="cypress" />
 
-import LoginPage from '../pages/api.page';
+import LoginPage from '../pages/login.page';
+import UsuariosPage from '../pages/usuarios.page'
 
 describe('ServeRest Tests - API', () => {
 
   it('Deve realizar login com sucesso via API', () => {
     LoginPage.login()
   });
+
+  it('Pegar dados dos usuarios - GET', () => {
+    UsuariosPage.usuarios()
+  })
 });
