160b2a61a65aebd665b16ff54b4cd497811b1458
Hrqlv <henriquelopesvelozo@gmail.com>
2024-06-20 16:09:08 -0300

    Feito a api de cadastrar um usuario - POST

--- cypress/e2e/pages/usuarios.page.js
+++ cypress/e2e/pages/usuarios.page.js
@@ -3,7 +3,7 @@ class UsuariosPage {
         this.url = 'https://serverest.dev';
       }
 
-    usuarios() {
+    pegarListaUsuarios() {
       return cy.request({
         method: 'GET',
         url: `${this.url}/usuarios`, 
@@ -14,7 +14,27 @@ class UsuariosPage {
         expect(response.status).to.eq(200);
       });
     }
+
+    cadastrarUsuario(name, email, password, adm) {
+        return cy.request({
+            method: 'POST',
+            url: `${this.url}/usuarios`,
+            body: {
+              nome: name,
+              email: email,
+              password: password,
+              administrador: adm
+            },
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then((response) => {
+            expect(response.status).to.eq(201);
+            expect(response.body.message).to.eq('Cadastro realizado com sucesso');
+            Cypress.env('authToken', response.body.token);
+          });
   }
+}
   
   export default new UsuariosPage();
   
